3d757d6921825ef8486d58f39311fb79c8e32575
Wester111 <144542488+Wester111@users.noreply.github.com>
2023-09-23 12:34:03 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Math.Pow(x,2));
+            int y = 7 * a + 3 * x + 6;
+
+            Console.WriteLine(y);
+
+
+        }
+    }
+}
